7dd4fc0f0d1ceea56e995719c093f16c666fcc51
Jarred Myers <jarredm1999@gmail.com>
2020-10-20 23:34:21 +0000

    Filled in all of the base files with limited functionality

--- logic.js
+++ logic.js
@@ -1,1 +1,30 @@
 //This is where all of the server side functions and logic will be held
+const express = require('express');
+const app = express();
+let port = process.env.PORT;
+if (port == null || port == "") {
+    port = 80;
+}
+
+function initial(res) {
+    let args = {
+        "status" : "You have not logged in"
+    };
+    res.render('index', args);
+}
+
+function login(req, res){
+    console.log(req.params);
+}
+
+
+app.use(express.static("static"));
+app.set('views', './views')
+app.set('view engine', 'pug')
+app.get('/', (req, res) => {
+    initial(res);
+});
+app.get('/login/email/:email/password/:password', login);
+app.listen(port, ()=> {
+    console.log("App running at port=" + port)
+});
--- static/index.js
+++ static/index.js
@@ -1,1 +1,27 @@
 // This is where all of the client side functions and logic will go
+console.log("Hello World");
+
+function send(email, password) {
+    let url = "/login/email/" + email + "/password/" + password;
+    console.log(url);
+    window.location.replace(url);
+}
+
+function login(email, password) {
+    if (email == 'jarredm1999@gmail.com' && password == 'pass123') {
+        send(email, password);
+    } else {
+        alert("You entered an invalid username or password");
+        return;
+    }
+}
+
+function setup() {
+    $("#login").click(login);
+    $("#signup").click(signup);
+}
+
+
+
+
+$(document).ready(setup);
